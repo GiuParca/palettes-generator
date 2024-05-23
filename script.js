074664c7f18a3f8cc1e962e08d569e46a73a199b
Giulia Parca <124640350+GiuParca@users.noreply.github.com>
2024-05-23 23:55:03 +0100

    pallete generator

---
+++ script.js
@@ -0,0 +1,33 @@
+const buttons = document.querySelectorAll(".change-color");
+const canvas = document.querySelectorAll(".div-color");
+const buttonRandom = document.getElementById("random-pallete");
+
+
+canvas.forEach(div => {
+    const button = div.querySelector(".change-color");
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+        let hexRef = `${getHexRef()}`;
+        div.style.background = hexRef;
+        const p = div.querySelector("p");
+        p.innerText = hexRef;
+    })
+})
+
+function getHexRef() {
+    const hexCharacters = "0123456789abcdef";
+    let hexRef = "#";
+
+    for (let i = 0; i < 6; i++) {
+        let index = Math.floor(Math.random() * hexCharacters.length)
+        let character = hexCharacters[index];
+        hexRef += character;
+    }
+    return hexRef;
+}
+
+buttonRandom.addEventListener('click', (event) =>{
+    event.preventDefault();
+    buttons.forEach(button => button.click());
+})
+
